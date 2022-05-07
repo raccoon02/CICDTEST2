566ad4413b1700ebfce0a38118ace77f98759440
KimSungJin <human9802@gmail.com>
2022-05-08 02:21:21 +0900

    배포 V.7

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,6 @@ cp $REPOSITORY/*.jar $REPOSITORY/
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | head -n 1)
 
-
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
@@ -28,4 +27,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-${IDLE_PROFILE}.properties \
     -Dspring.profiles.active=${IDLE_PROFILE} \
-    ${JAR_NAME} > ${REPOSITORY}/nohup.out 2>&1 &
+     corinne_BE-0.0.1-SNAPSHOT.jar nohup.out 2>&1 &
